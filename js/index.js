0b261fd7a49ac62a7e6bc17732f668cc74ba0ef9
dani320 <flo18020@byui.edu>
2021-04-24 22:10:56 -0500

    final

--- js/index.js
+++ js/index.js
@@ -1,2 +1,4 @@
 const date = new Date();
+const year = date.getFullYear();
+document.querySelector('#theyear').textContent = year;
 document.querySelector('#lastmod').textContent = document.lastModified;
